b066646f98387f5fe697b6040fcb8029be960dd6
Gongzai Li <ligongzainz@gmail.com>
2021-12-16 15:27:32 +1300

    # set up router -- done

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import {createWebHistory, createRouter} from 'vue-router'
+
+const routes = [{path: "/", redirect: '/home'}]
+
+
+const router = createRouter({
+    routes, 
+    history: createWebHistory()
+})
+
+
+export default router
